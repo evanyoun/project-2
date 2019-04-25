58c0d5d84c5d88d93204c7c9ca442d375334e295
evany <evany@uoregon.edu>
2019-04-25 13:14:45 -0700

    lab3 backup save

---
+++ client/javascripts/articles-api.js
@@ -0,0 +1,61 @@
+// jshint esversion: 6
+
+let main = function() {
+  /*
+    let prefixURL = "http://api.flickr.com/services/feeds/photos_public.gne?tags=";
+    let suffixURL = "&format=json&jsoncallback=?";
+    //get value entered by user from textbox
+    let flickrTag = $("input").???();
+    let requestURL = prefixURL + flickrTag + suffixURL;
+
+    //clear old photos
+    $(".photos").???("");
+*/
+  let requestURL = "https://thejsway-server.herokuapp.com/api/articles";
+
+  $.getJSON(requestURL, function(response) {
+    console.log(response);
+    console.log(`Article ${response[0].id}
+Title: ${response[0].title}
+Content: ${response[0].content}`);
+
+    response.forEach(function(json_obj) {
+      //make some elements
+      let div = document.createElement("div");
+      let header = document.createElement("h3");
+      let p = document.createElement("p");
+      header.textContent = `Article ${json_obj.id}`;
+
+      //put elements inside each other
+      div.appendChild(header);
+
+      let container = document.getElementsByClassName("articles")[0];
+      container.appendChild(div);
+    });
+    /*
+    flickrResponse.items.forEach(function(item, index) {
+
+      //Flickr returns 20 images by default
+      //We need only six images for the Gallery
+      if (index < 6) {
+        // create a new JQuery element to hold the image
+        // but hide it so we can fade it in
+        let $img = $("<img>").hide();
+
+        // set the attribute to the url
+        // contained in the response
+        $img.attr("src", item.media.m);
+        $img.attr("width", "100");
+
+        // attach the img tag to the main
+        // photos element and then fade it in
+        $(".photos").append($img);
+        $img.fadeIn();
+      }
+    });
+
+  */
+  });
+};
+
+$(document).ready(main);
