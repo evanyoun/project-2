e3c1f5c34e01581b522442b989a42dd3811dd1c3
evany <evany@uoregon.edu>
2019-04-26 16:52:34 -0700

    finished reverseAllWords/2

--- client/javascripts/functions.js
+++ client/javascripts/functions.js
@@ -1,12 +1,22 @@
 // jshint esversion: 6
-
-let reverseAllWord = send => {
+//3-a
+let reverseAllWords = send => {
   let result = "";
-  let words = send.split(" ");
+  let words = send.split("");
 
   //reverse all words in sentence
   for (var i = 0; i < words.length; i++) {
-    result = result + reverse[i] + " ";
+    result = words[i] + result;
   }
+  return result;
+};
+
+//3-b
+let reverseAllWord2 = send => {
+  let result = "";
+  let words = send.split("");
+  words.forEach(function(c) {
+    result = c + result;
+  });
   return result;
 };
